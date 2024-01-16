e3b33bb723afbf9291c05cecee9c91d50cefb5d2
David Smith <cpmpdavidsmith@icloud.com>
2024-01-16 14:18:42 -0500

    prac

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Nullable
+{
+    class Program
+    {
+        static void Main()
+        {
+            int? x = null;
+            int? y = 11;
+            if (x== null)
+            {
+                Console.WriteLine(y.Value);
+            }
+            else
+            {
+                Console.WriteLine("Undefined");
+            }
+            Console.Read();
+        }
+    }
+}
